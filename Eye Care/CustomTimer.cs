a1a7b8ff45d2237d931fb179d778b5a3134ac909
Hawad Ahmad <hawadjan.1@gmail.com>
2021-05-29 23:11:01 -0400

    Add project files.

---
+++ Eye Care/CustomTimer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Eye_Care
+{
+    class CustomTimer : System.Timers.Timer
+    {
+        private System.Timers.Timer timer;
+        private NotifyIcon notifyIcon;
+        private int sec;
+        private Boolean isCountingDown = false;
+        private int count = 0;
+        private int remainingMin = 0;
+        public CustomTimer(NotifyIcon notifyIcon, int sec)
+        {
+            this.notifyIcon = notifyIcon;
+            this.sec = sec;
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            isCountingDown = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (!isCountingDown)
+                return;
+
+            if (count % 60 == 0)
+            {
+                remainingMin = (sec - count) / 60;
+                updateToolTipText(remainingMin);
+            }
+
+            if (count == sec)
+            {
+                notifyIcon.ShowBalloonTip(0, "Eye Care", "Take a break form your screen", ToolTipIcon.Info);
+                count = 0;
+                remainingMin = sec / 60;
+                updateToolTipText(remainingMin);
+            }
+            Console.WriteLine(count);
+            count++;
+        }
+
+        private void updateToolTipText(int remainingMin)
+        {
+            notifyIcon.Text = "Eye Care. " + remainingMin + " minutes remaining";
+        }
+
+        public void Pause()
+        {
+            isCountingDown = false;
+        }
+
+        public void Resume()
+        {
+            isCountingDown = true;
+        }
+
+        public Boolean timerIsCountingDown()
+        {
+            return isCountingDown;
+        }
+
+        public int getRemainingMin()
+        {
+            return remainingMin;
+        }
+    }
+}
---
+++ Eye Care/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Eye_Care;
+using Eye_Care.Properties;
+
+public class Form1 : System.Windows.Forms.Form
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new MyCustomApplicationContext());
+    }
+}
+
+
+public class MyCustomApplicationContext : ApplicationContext
+{
+    private NotifyIcon trayIcon;
+    private CustomTimer cTimer;
+
+    public MyCustomApplicationContext()
+    {
+        // Initialize Tray Icon
+        trayIcon = new NotifyIcon();
+        trayIcon.Icon = Resources.eye;
+
+        ContextMenu cm = new ContextMenu();
+        cm.MenuItems.Add(new MenuItem("Pause", Control));
+        cm.MenuItems.Add("-");
+        cm.MenuItems.Add(new MenuItem("Exit", Exit));
+
+        trayIcon.ContextMenu = cm;
+        trayIcon.Visible = true;
+
+
+        cTimer = new CustomTimer(trayIcon, 20*60);
+    }
+
+    void Exit(object sender, EventArgs e)
+    {
+        // Hide tray icon, otherwise it will remain shown until user mouses over it
+        trayIcon.Visible = false;
+
+        Application.Exit();
+    }
+
+    void Control(object sender, EventArgs e)
+    {
+        // Change text the menuItem text
+        MenuItem menuItem = (MenuItem)sender;
+
+        if (cTimer.timerIsCountingDown())
+        {
+            cTimer.Pause();
+            menuItem.Text = "Resume";
+            trayIcon.Text = "Eye Care. Paused";
+        }
+        else
+        {
+            cTimer.Resume();
+            menuItem.Text = "Pause";
+            trayIcon.Text = "Eye Care. " + cTimer.getRemainingMin() + " minutes remaining";
+        }
+    }
+}
