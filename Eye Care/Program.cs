86b95f875eb9cffca5d1838d9e9e147fdde00706
Hawad Ahmad <hawadjan.1@gmail.com>
2021-05-31 17:14:29 -0400

    cannot run multiple instances of this application
    Checks if a file exists. If it exists, exit the app. Otherwise, start the app and create the lockfile. After exiting the app, it deletes the lockfile.

--- Eye Care/Program.cs
+++ Eye Care/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
 using Eye_Care;
@@ -6,11 +7,21 @@ using Eye_Care.Properties;
 
 public class Form1 : System.Windows.Forms.Form
 {
+    public static string path = @"c:\temp\lockfile";
     [STAThread]
     static void Main()
     {
+        // check if there's already an instance running
+        if (File.Exists(path)) // if the lockfile exists, show message
+        {
+            MessageBox.Show("An instance of Eye care is already running");
+            return;
+        }
+            
+        File.Create(path).Close();
         Application.Run(new MyCustomApplicationContext());
     }
+
 }
 
 
@@ -25,16 +36,26 @@ public class MyCustomApplicationContext : ApplicationContext
         trayIcon = new NotifyIcon();
         trayIcon.Icon = Resources.eye;
 
+        // create contextmenu and add items
         ContextMenu cm = new ContextMenu();
         cm.MenuItems.Add(new MenuItem("Pause", Control));
         cm.MenuItems.Add("-");
         cm.MenuItems.Add(new MenuItem("Exit", Exit));
 
+        // add contextmenu to trayIcon and show it
         trayIcon.ContextMenu = cm;
         trayIcon.Visible = true;
 
-
+        // start the timer
         cTimer = new CustomTimer(trayIcon, 20*60);
+
+        // add event handler when the application exits
+        Application.ApplicationExit += new EventHandler(this.onApplicationExit);
+    }
+    void onApplicationExit(object sender, EventArgs e)
+    {
+        // delete the lockfile
+        File.Delete(Form1.path);
     }
 
     void Exit(object sender, EventArgs e)
@@ -47,18 +68,22 @@ public class MyCustomApplicationContext : ApplicationContext
 
     void Control(object sender, EventArgs e)
     {
-        // Change text the menuItem text
+        // get the menuItem sender
         MenuItem menuItem = (MenuItem)sender;
 
         if (cTimer.timerIsCountingDown())
         {
+            // pause timer
             cTimer.Pause();
+            // update text
             menuItem.Text = "Resume";
             trayIcon.Text = "Eye Care. Paused";
         }
         else
         {
+            // resume timer
             cTimer.Resume();
+            // update text
             menuItem.Text = "Pause";
             trayIcon.Text = "Eye Care. " + cTimer.getRemainingMin() + " minutes remaining";
         }
